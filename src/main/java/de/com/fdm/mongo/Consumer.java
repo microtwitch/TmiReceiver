b22eea0036b22b89146e335313d6ad4624066752
M4tthewDE <mattider2@gmail.com>
2021-12-26 14:49:56 +0100

    fix duplicate entries in db

--- src/main/java/de/com/fdm/mongo/Consumer.java
+++ src/main/java/de/com/fdm/mongo/Consumer.java
@@ -1,14 +1,25 @@
 package de.com.fdm.mongo;
 
+import org.bson.types.ObjectId;
+
 import java.util.List;
 
 public class Consumer {
+    private ObjectId _id;
     private final List<String> channels;
     private final String callback;
 
     public Consumer(List<String> channels, String callback) {
         this.channels = channels;
         this.callback = callback;
+    }
+
+    public ObjectId get_id() {
+        return _id;
+    }
+
+    public void set_id(ObjectId _id) {
+        this._id = _id;
     }
 
     public List<String> getChannels() {
@@ -20,6 +31,11 @@ public class Consumer {
     }
 
     public void addChannel(List<String> channels) {
-        this.channels.addAll(channels);
+        for (String channel : channels) {
+            if (this.channels.contains(channel)) {
+                continue;
+            }
+            this.channels.add(channel);
+        }
     }
 }
