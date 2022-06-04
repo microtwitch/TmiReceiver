8c811ca9b8f57ae3398beac4ca12ec03eafe1656
M4tthewDE <mattider2@gmail.com>
2022-06-04 19:29:11 +0200

    leave channels that dont have clients listening

--- src/main/java/de/com/fdm/tmi/Reader.java
+++ src/main/java/de/com/fdm/tmi/Reader.java
@@ -17,6 +17,9 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 @Component
 public class Reader {
@@ -25,9 +28,12 @@ public class Reader {
     private final RedissonClient redissonClient;
     private final ObjectMapper mapper;
 
+    private final HashMap<String, Queue<Long>> channelUsageHistory;
+
     @Autowired
     public Reader() throws IOException {
         this.mapper = new ObjectMapper();
+        this.channelUsageHistory = new HashMap<>();
 
         Config config = Config.fromYAML(new File("src/main/resources/redisson_config.yaml"));
         this.redissonClient = Redisson.create(config);
@@ -39,13 +45,36 @@ public class Reader {
 
     private void handleChannelMessage(ChannelMessageEvent event) {
         TwitchMessageDto twitchMessageDto = TwitchMessageDto.fromEvent(event);
+        String channel = twitchMessageDto.channel();
 
         RTopic topic = redissonClient.getTopic("tmiReceiver." + event.getChannel().getName());
         try {
-            topic.publish(mapper.writeValueAsString(twitchMessageDto));
+            if (!isChannelUsed(channel)) {
+                twitchChat.leaveChannel(channel);
+            }
+
+            long numClients = topic.publish(mapper.writeValueAsString(twitchMessageDto));
+
+            if (channelUsageHistory.get(channel).size() == 5) {
+                channelUsageHistory.get(channel).remove();
+            }
+            channelUsageHistory.get(channel).add(numClients);
         } catch (JsonProcessingException e) {
             LOG.error(e.getMessage());
         }
+    }
+
+    private boolean isChannelUsed(String channel) {
+        if (channelUsageHistory.get(channel).size() != 5) {
+            return true;
+        }
+
+        for (Long entry : channelUsageHistory.get(channel)) {
+            if (entry != 0) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void handleMeMessage(ChannelMessageActionEvent event) {
@@ -59,7 +88,11 @@ public class Reader {
         this.handleChannelMessage(messageEvent);
     }
 
+    // twitch4j handles joining the same chat multiple times
+    // the join gets ignored in that case!
     public void joinChannel(String channel) {
+        channelUsageHistory.put(channel, new ArrayBlockingQueue<>(5));
+
         twitchChat.joinChannel(channel);
     }
 }
