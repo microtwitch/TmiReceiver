bb0330215f5059c82b71e9d029fd1ec527c30c34
M4tthewDE <mattider2@gmail.com>
2022-06-04 16:16:01 +0200

    add more parameters to twitch messages

--- src/main/java/de/com/fdm/tmi/TwitchMessageDto.java
+++ src/main/java/de/com/fdm/tmi/TwitchMessageDto.java
@@ -2,8 +2,13 @@ package de.com.fdm.tmi;
 
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
 
-public record TwitchMessageDto(String channel, String message) {
+public record TwitchMessageDto(String channel, String message, String userId, String rawMsg) {
     public static TwitchMessageDto fromEvent(ChannelMessageEvent event) {
-        return new TwitchMessageDto(event.getChannel().getName(), event.getMessage());
+        return new TwitchMessageDto(
+                event.getChannel().getName(),
+                event.getMessage(),
+                event.getUser().getId(),
+                event.getMessageEvent().getRawMessage()
+        );
     }
 }
